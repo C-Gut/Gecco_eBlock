3db4c13426842433cbf0984d9afcea360ddb37dd
C-Gut <mag23mj@hotmail.com>
2023-09-20 18:39:07 +0200

    now it takes multiple sequences in fasta format . Detects bsai sites, substitutes corresponding codons to give a sequence without bsai sites for each input sequence. The output is also in fasta format

--- server.R
+++ server.R
@@ -13,7 +13,6 @@ sumoBB2 <-"GGCCCGAACAAAAACTCATCTCAGAAGAGGATCTGAATAGCGCCGTCGACCATCATCATCATCATCATT
 #   sample(c('A', 'C', 'T', 'G'), n, replace = TRUE) %>% paste0(collapse = '')
 # }
 
-####
 
 # Function to parse FASTA format and create a data frame
 parse_fasta <- function(input_text) {
@@ -23,42 +22,22 @@ parse_fasta <- function(input_text) {
   
   # Split the input text into lines
   lines <- unlist(strsplit(input_text, "\n"))
+  seq_names <- lines[str_detect(lines, "^>")]
+  seq_data <- lines[!str_detect(lines, "^>")] 
   
-  # Initialize variables to keep track of the current sequence name and sequence data
-  current_name <- ""
-  current_data <- ""
-  
-  # Iterate through each line in the input text
-  for (line in lines) {
-    # Check if the line starts with ">"
-    if (substr(line, 1, 1) == ">") {
-      # If a sequence is already being processed, store it
-      if (current_name != "") {
-        seq_names <- c(seq_names, current_name)
-        seq_data <- c(seq_data, current_data)
-      }
-      
-      # Set the current sequence name and reset sequence data
-      current_name <- substr(line, 2, nchar(line))
-      current_data <- ""
-    } else {
-      # Append the line to the current sequence data
-      current_data <- paste(current_data, line, sep = "")
-    }
+  if (length(seq_names) != length(seq_data)) {
+    print("problem with sequence")
+    return()
   }
   
-  # Add the last sequence to the vectors
-  if (current_name != "") {
-    seq_names <- c(seq_names, current_name)
-    seq_data <- c(seq_data, current_data)
-  }
   
   # Create a data frame from the vectors
   fasta_df <- data.frame(Name = seq_names, Sequence = seq_data)
   # Create a list from the vectors
-  fasta_list <- list(Name = seq_names, Sequence = seq_data)
-  
-  return(fasta_df)
+  fasta_list <- as.list(seq_data)
+  names(fasta_list) <- seq_names
+
+  return(list(fasta_list, fasta_df))
 }
 
 ###
@@ -107,12 +86,13 @@ bsai_locate <- function(seq) {
   
   combined_df <- combined_df[order(combined_df$start), ]
   
+  combined_df <- combined_df[!is.na(combined_df$start),]
   return(combined_df)
 }
 
 
 # Use lapply to apply the bsai_locate function to each sequence in the list
-#result_list <- lapply(fasta_list, bsai_locate)
+#result_list <- lapply(fasta_df$seq_data, bsai_locate)
 
 # Print the resulting list of data frames
 #print(result_list)
@@ -153,7 +133,7 @@ server <- function(input, output, session) {
   })
   
   output$fasta_table <- renderTable({
-    parsed_data()
+    parsed_data()[[2]]
   })
   
   # for testing; create random sequences of length specified in input$ex_len
@@ -166,136 +146,157 @@ server <- function(input, output, session) {
   # }, ignoreInit = TRUE)
   
   # process the sequence: upper case, detect bsai sites, find position within codon, suggest swapped codon
-
-  processed_input <- reactive({
-    df <- bsai_locate(seq = toupper(input$fasta_input))
+  
+  bsai_table <- function(seq) {
+    df <- bsai_locate(seq = toupper(seq))
     df$first_cod_to_change <- ifelse(df$dir == 'fw',
                                      c('GTC', 'GGT', 'TCT')[(df$cod_pos %% 3) + 1],
                                      c('AGA', 'GAG', 'GAC')[(df$cod_pos %% 3) + 1])
     df$change_cod_to <- ifelse(df$dir == 'fw',
                                c('GTG', 'GGC', 'TCC')[(df$cod_pos %% 3) + 1],
                                c('AGG', 'GAA', 'GAT')[(df$cod_pos %% 3) + 1])
-
-# Create a new column with the position on the sequence where the codon that has to be changed starts.
     
-     for (i in 1:nrow(df)) {
-       if (df$cod_pos[i] == 3)
-         df$calc_pos[i] <- df$start[i] + 1
-       
-       if (df$cod_pos[i] == 2)
-         df$calc_pos[i] <- df$start[i] + 2
-       
-       if (df$cod_pos[i] == 1)
-         df$calc_pos[i] <- df$start[i]
-     }
+    # Create a new column with the position on the sequence where the codon that has to be changed starts.
 
-   
+    for (i in seq_len(nrow(df))) {
+      if (df$cod_pos[i] == 3)
+        df$calc_pos[i] <- df$start[i] + 1
+      
+      if (df$cod_pos[i] == 2)
+        df$calc_pos[i] <- df$start[i] + 2
+      
+      if (df$cod_pos[i] == 1)
+        df$calc_pos[i] <- df$start[i]
+    }
+    df
+  }
+  
+  modified_seq <- function(df, seq) {
     # Replace codons in the input sequence
-    modified_sequence <- toupper(input$fasta_input)
-
-     for (i in 1:nrow(df)) {
-       start_pos <- df[i, "start"]
-       end_pos <- df[i, "end"]
-       replacement <- df[i, "change_cod_to"]
-       pos_in_seq <- df[i, "calc_pos"]
-
-     modified_sequence <- paste0(substring(modified_sequence, 1, pos_in_seq - 1),
-                                 replacement,
-                                 substring(modified_sequence, pos_in_seq + 3))
-     }
+    modified_sequence <- seq
+    for (i in seq_len(nrow(df))) {
+      start_pos <- df[i, "start"]
+      end_pos <- df[i, "end"]
+      replacement <- df[i, "change_cod_to"]
+      pos_in_seq <- df[i, "calc_pos"]
+      
+      modified_sequence <- paste0(substring(modified_sequence, 1, pos_in_seq - 1),
+                                  replacement,
+                                  substring(modified_sequence, pos_in_seq + 3))
+    }
+    modified_sequence
+  }
+  
+  processed_input <- reactive({
+    seq_list <- parsed_data()[[1]]
+    list_bsai_dfs <- lapply(seq_list, function(x) {
+      df <- bsai_table(x)
+      modified_sequence <- modified_seq(df, x)
+      list(df, modified_sequence)
+      
+    })
     
-    list(df, modified_sequence)
+    seqs <- lapply(list_bsai_dfs, function(x) x[[2]])
+    
+    fasta_out <-
+      c(rbind(names(seqs), as.character(unlist(seqs)))) %>% paste(collapse = '\n')
+    updateTextInput(session = getDefaultReactiveDomain(),
+                                      inputId = 'mod_seq',
+                                      value = fasta_out)
+    
+    list_bsai_dfs
     })
 
+  observe(processed_input())
   
   # output table for bsai sites
   # filling a new text input field with the new sequence
-  output$bsai_table <- renderDT({
-    updateTextInput(session = getDefaultReactiveDomain(),
-                    inputId = 'mod_seq',
-                    value = processed_input()[[2]])
-    datatable(processed_input()[[1]], options = list(dom = 't'))  # option removes (here) pointless search field
-  })
+  # output$bsai_table <- renderDT({
+  #   updateTextInput(session = getDefaultReactiveDomain(),
+  #                   inputId = 'mod_seq',
+  #                   value = processed_input())
+  #  datatable(processed_input()[[1]], options = list(dom = 't'))  # option removes (here) pointless search field
+  # })
 
   
-  # output table for fragments
-  output$frag_table <- renderDT({
-    fragm.df <- split_seq_in_chunks(seq = input$mod_seq, max_len = as.numeric(input$frag_len))
-    fragm.df$p5_Bsa <- BsaMid1
-    fragm.df$p3_Bsa <- BsaMid2
-    fragm.df[1, "p5_Bsa"] <- BsaTGGT
-    fragm.df[nrow(fragm.df), "p3_Bsa"] <- BsaSTOPCTTG  
-    fragm.df$p5_overhang <- substr(fragm.df$fragments, 1, 4)
-    #fragm.df$p3_overhang <- substr(fragm.df$fragments, nchar(fragm.df$fragments) - 3, nchar(fragm.df$fragments))
-    print(fragm.df[, "p5_overhang"])
-    ### checks overhangs  
-      #1# are all overhangs unique?
-    
-        # Find duplicated values and report row numbers
-    
-    # Define the columns you want to compare for uniqueness
-    column1_to_check <- fragm.df$p5_overhang
-    column2_to_check <- fragm.df$p3_overhang
-    
-    # Combine both columns into a single vector for comparison
-    combined_column <- c(column1_to_check, column2_to_check)
-    
-    # Find duplicated values and report row numbers
-    duplicated_rows <- which(duplicated(column1_to_check) | duplicated(column1_to_check, fromLast = TRUE))
-    
-    # Check if there are any duplicated values
-    if (length(duplicated_rows) > 0) {
-      cat("Duplicate values found in the following rows:\n")
-      for (row_num in duplicated_rows) {
-        cat("Row", row_num, ":", combined_column[row_num], "\n")
-        fragm.df$p5_overhang_check_unique[i] <- FALSE
-      }
-    } else {
-      cat("No duplicate values found between the two columns.\n")
-      fragm.df$p5_overhang_check_unique <- TRUE
-      
-    }
-    
-      #2# is any overhang palindromic?
-
-    # Function to check if a DNAString is palindromic
-    is_palindromic <- function(sequence) {
-      complement_sequence <- reverseComplement(DNAString(sequence))
-      identical(sequence, as.character(complement_sequence))
-    }
-    
-    # Check if each sequence in the 'Sequence' column is palindromic
-    fragm.df$p5_overhang_check_palindrome <- sapply(fragm.df$p5_overhang, function(seq) !is_palindromic(seq))
-    
-
-      #3# does any overhang have more than 2 repeats?   
-   
-    # Function to check for repeated characters more than twice
-    has_repeated_characters <- function(text) {
-      any(rle(strsplit(text, "")[[1]])$lengths > 2)
-    }
-    # Check if each value in the 'Text' column has repeated characters more than twice
-    fragm.df$p5_overhang_check_repeats <- sapply(fragm.df$p5_overhang, has_repeated_characters)
-    
-    # Invert the 'repeat' column, so it's TRUE for rows with repeats and FALSE for others
-    fragm.df$p5_overhang_check_repeats <- !fragm.df$p5_overhang_check_repeats
-
-    #4# pass all checks?
-    
-    # Create a new column 'test' based on the conditions
-    fragm.df$test <- ifelse(fragm.df$p5_overhang_check_unique & fragm.df$p5_overhang_check_palindrome & fragm.df$p5_overhang_check_repeats, TRUE, FALSE)
-    
-    ### this only if all checks are true, otherwise change fragments
-    # Create another data frame by pasting values from fragm.df
-    
-    full_fragm.df <- data.frame(full_fragm = NA)
-    
-    #full_fragm.df$full_fragm <- ifelse(fragm.df$test, paste(fragm.df$p5_Bsa, fragm.df$p5_overhang, fragm.df$fragments, fragm.df$p3_Bsa), xxxxxxxxxxx)
-  print(full_fragm.df)  
-  fragm.df
-  #list(fragm.df, full_fragm.df)
-  })
-  
+  # # output table for fragments
+  # output$frag_table <- renderDT({
+  #   fragm.df <- split_seq_in_chunks(seq = input$mod_seq, max_len = as.numeric(input$frag_len))
+  #   fragm.df$p5_Bsa <- BsaMid1
+  #   fragm.df$p3_Bsa <- BsaMid2
+  #   fragm.df[1, "p5_Bsa"] <- BsaTGGT
+  #   fragm.df[nrow(fragm.df), "p3_Bsa"] <- BsaSTOPCTTG  
+  #   fragm.df$p5_overhang <- substr(fragm.df$fragments, 1, 4)
+  #   #fragm.df$p3_overhang <- substr(fragm.df$fragments, nchar(fragm.df$fragments) - 3, nchar(fragm.df$fragments))
+  #   print(fragm.df[, "p5_overhang"])
+  #   ### checks overhangs  
+  #     #1# are all overhangs unique?
+  #   
+  #       # Find duplicated values and report row numbers
+  #   
+  #   # Define the columns you want to compare for uniqueness
+  #   column1_to_check <- fragm.df$p5_overhang
+  #   column2_to_check <- fragm.df$p3_overhang
+  #   
+  #   # Combine both columns into a single vector for comparison
+  #   combined_column <- c(column1_to_check, column2_to_check)
+  #   
+  #   # Find duplicated values and report row numbers
+  #   duplicated_rows <- which(duplicated(column1_to_check) | duplicated(column1_to_check, fromLast = TRUE))
+  #   
+  #   # Check if there are any duplicated values
+  #   if (length(duplicated_rows) > 0) {
+  #     cat("Duplicate values found in the following rows:\n")
+  #     for (row_num in duplicated_rows) {
+  #       cat("Row", row_num, ":", combined_column[row_num], "\n")
+  #       fragm.df$p5_overhang_check_unique[i] <- FALSE
+  #     }
+  #   } else {
+  #     cat("No duplicate values found between the two columns.\n")
+  #     fragm.df$p5_overhang_check_unique <- TRUE
+  #     
+  #   }
+  #   
+  #     #2# is any overhang palindromic?
+  # 
+  #   # Function to check if a DNAString is palindromic
+  #   is_palindromic <- function(sequence) {
+  #     complement_sequence <- reverseComplement(DNAString(sequence))
+  #     identical(sequence, as.character(complement_sequence))
+  #   }
+  #   
+  #   # Check if each sequence in the 'Sequence' column is palindromic
+  #   fragm.df$p5_overhang_check_palindrome <- sapply(fragm.df$p5_overhang, function(seq) !is_palindromic(seq))
+  #   
+  # 
+  #     #3# does any overhang have more than 2 repeats?   
+  #  
+  #   # Function to check for repeated characters more than twice
+  #   has_repeated_characters <- function(text) {
+  #     any(rle(strsplit(text, "")[[1]])$lengths > 2)
+  #   }
+  #   # Check if each value in the 'Text' column has repeated characters more than twice
+  #   fragm.df$p5_overhang_check_repeats <- sapply(fragm.df$p5_overhang, has_repeated_characters)
+  #   
+  #   # Invert the 'repeat' column, so it's TRUE for rows with repeats and FALSE for others
+  #   fragm.df$p5_overhang_check_repeats <- !fragm.df$p5_overhang_check_repeats
+  # 
+  #   #4# pass all checks?
+  #   
+  #   # Create a new column 'test' based on the conditions
+  #   fragm.df$test <- ifelse(fragm.df$p5_overhang_check_unique & fragm.df$p5_overhang_check_palindrome & fragm.df$p5_overhang_check_repeats, TRUE, FALSE)
+  #   
+  #   ### this only if all checks are true, otherwise change fragments
+  #   # Create another data frame by pasting values from fragm.df
+  #   
+  #   full_fragm.df <- data.frame(full_fragm = NA)
+  #   
+  #   #full_fragm.df$full_fragm <- ifelse(fragm.df$test, paste(fragm.df$p5_Bsa, fragm.df$p5_overhang, fragm.df$fragments, fragm.df$p3_Bsa), xxxxxxxxxxx)
+  # print(full_fragm.df)  
+  # fragm.df
+  # #list(fragm.df, full_fragm.df)
+  # })
+  # 
   # return some info about fragments as text
   output$total_len <- renderText({
     frag_input_seq <- input$mod_seq
