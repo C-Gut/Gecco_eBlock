92185d60582a9007c2e67caf32f2f5ec2e14cbb6
C-Gut <mag23mj@hotmail.com>
2023-09-20 19:24:17 +0200

    nicer layout

--- ui.R
+++ ui.R
@@ -14,26 +14,21 @@ ui <- fluidPage(
   # Horizontal line
   hr(),
 
-  # Show text input box to paste fasta sequence and display the parsed sequence as a table
-  sidebarLayout(
-    sidebarPanel(
-      helpText("Paste your FASTA sequences below:"),
-      textAreaInput(inputId = "fasta_input", value =
-#Seq1 has 2 GGTCTC in 15 and 37, Seq3 has 1 gagacc in 15
+# Show text input box to paste fasta sequence and display the parsed sequence as a table
+  helpText("Paste your FASTA sequences below:"),
+  #Seq1 has 2 GGTCTC in 15 and 37, Seq3 has 1 gagacc in 15
+  textAreaInput(inputId = "fasta_input", value =
 ">Seq1
 ATGCTAGCTAGCTAGGTCTCTAGCTAGCTAGCTAGCGGTCTCTAGCTAGCTAGC
 >Seq2
 CGATCGATCGATCGATCGATCGATCGATCGATCGATCGATCGATCGATCGATCGATCGAT
 >Seq3
-TACGTACGTACGTACGAGACCTTCGTACGTACGTACGTACGTACGTACGTACGTACGTACG", label = "Sequence(s)", placeholder = "Paste FASTA sequences here..."),
-      actionButton("parse_button", "Parse")
-    ),
-    
-    mainPanel(
-      tableOutput("fasta_table")
-    )
-  ),
-  
+TACGTACGTACGTACGAGACCTTCGTACGTACGTACGTACGTACGTACGTACGTACGTACG", 
+label = "Sequence(s)", placeholder = "Paste FASTA sequences here...", width = '100%', height = '20vh'),
+  actionButton("parse_button", "Parse"),
+
+  tableOutput("fasta_table"),
+
   # # Create a text input for the DNA sequence to be introduced by the user
   # textInput('ex_len', 'example seq length', value = 41),
   # textInput('frag_len', 'eblock fragment max. length', value = 870),
