ff351306c62a87aedc506830e6ffe397a25c2be1
C-Gut <mag23mj@hotmail.com>
2023-11-10 11:57:00 +0100

    download button for fragments.df, commented out for now

--- ui.R
+++ ui.R
@@ -38,7 +38,8 @@ TACGTACGTACGTACGAGACCTTCGTACGTACGTACGTACGTACGTACGTACGTACGTACG",
       br(), 
       # # Create a text input for the DNA sequence to be introduced by the user
       textInput('frag_len', 'eblock fragment max. length', value = 10),
-      DTOutput("frag_table")
+      DTOutput("frag_table"),
+      #downloadButton("downloadXLS_fragm", "Download Excel File"),
     ),
     
     # Right column (occupies the right half)
